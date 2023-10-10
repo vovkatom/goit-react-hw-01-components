e03ed3f64f10c70cbf8909e4aeb36b345700296f
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-10-10 12:35:17 +0300

    +++

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
-// import { FriendList } from 'components/FriendList/FriendList';
+import { FriendList } from 'components/FriendList/FriendList';
 // import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import user from '../../user.json';
 import data from '../../data.json';
-// import friends from '../../friends.json';
+import friends from '../../friends.json';
 // import transactions from '../../transactions.json';
 import { Container } from './App.styled';
 // import { GlobalStyle } from 'components/GlobalStyle';
@@ -14,7 +14,7 @@ export const App = () => {
     <Container>
       <Profile user={user} />
       <Statistics title="Upload stats" data={data} />
-
+      <FriendList friends={friends} />
     </Container>
   );
 };
