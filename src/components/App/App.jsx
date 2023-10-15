d43f1056886bebd86b9cd9621021312e6c516ad3
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-10-15 12:22:50 +0300

    renew

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-import user from '../../json/user.json';
-import data from '../../json/data.json';
-import friends from '../../json/friends.json';
-import transactions from '../../json/transactions.json';
+import user from '../../data/user.json';
+import data from '../../data/data.json';
+import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
 import { Container } from './App.styled';
 // import { GlobalStyle } from 'components/GlobalStyle';
 
