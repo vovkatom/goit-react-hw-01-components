12c112a1c0713d9bd925ca4bced84519f6e6db6d
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-10-15 11:39:50 +0300

    ++

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-import user from '../../user.json';
-import data from '../../data.json';
-import friends from '../../friends.json';
-import transactions from '../../transactions.json';
+import user from '../../json/user.json';
+import data from '../../json/data.json';
+import friends from '../../json/friends.json';
+import transactions from '../../json/transactions.json';
 import { Container } from './App.styled';
 // import { GlobalStyle } from 'components/GlobalStyle';
 
